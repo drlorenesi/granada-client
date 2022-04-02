022cdba1b8396786e4f6c4477e8438931f2ac907
Diego R. Lorenesi <drlorenesi@gmail.com>
2022-04-02 08:02:11 -0600

    Actualizar dependencias.

--- src/utils/exportToExcel.js
+++ src/utils/exportToExcel.js
@@ -1,6 +1,6 @@
 import Blob from 'blob';
 import { saveAs } from 'file-saver';
-import XLSX from 'xlsx';
+import { utils, write } from 'xlsx';
 
 // Function to convert spreadsheet to a binary data for download
 const s2ab = (s) => {
@@ -14,14 +14,11 @@ export default function exportToExcel(ref) {
   // Get table
   let table = ref.current; // table is the id
   // Convert the table to an Excel sheet
-  let wb = XLSX.utils.table_to_book(table, { sheet: 'Customer Report' });
+  let wb = utils.table_to_book(table, { sheet: 'Customer Report' });
   // Write sheet to blobl
-  let blob = new Blob(
-    [s2ab(XLSX.write(wb, { bookType: 'xlsx', type: 'binary' }))],
-    {
-      type: 'application/octet-stream',
-    }
-  );
+  let blob = new Blob([s2ab(write(wb, { bookType: 'xlsx', type: 'binary' }))], {
+    type: 'application/octet-stream',
+  });
   // Return sheet file
   return saveAs(blob, 'download.xlsx');
 }
